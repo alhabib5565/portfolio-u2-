e4e39689673926b6524d56db9f4e8645b93072de
alhabib5565 <alhabib5565@gmail.com>
2023-10-27 01:38:21 +0600

    skills section done

--- src/Componect/Skills.jsx
+++ src/Componect/Skills.jsx
@@ -12,6 +12,7 @@ import github from "../assets/github.png";
 import firebase from "../assets/firebase.png";
 import redux from '../assets/redux.svg'
 import SectionTitle from "./SectionTitle";
+import mongodb from '../assets/mongodb.svg'
 
 const Skills = () => {
     const techs = [
@@ -19,67 +20,85 @@ const Skills = () => {
             id: 1,
             src: html,
             title: "HTML",
-            style: "shadow-orange-500",
+            style: "orange-500",
+            experties: 'Expert'
         },
         {
             id: 2,
             src: css,
             title: "CSS",
-            style: "shadow-blue-500",
+            style: "blue-500",
+            experties: 'Expert'
         },
         {
             id: 3,
             src: tailwind,
             title: "Tailwind",
-            style: "shadow-sky-400",
+            style: "sky-400",
+            experties: 'Expert'
         },
         {
             id: 4,
             src: bootrap,
             title: "Bootrap",
-            style: "shadow-blue-400",
+            style: "blue-400",
+            experties: 'Expert'
         },
         {
             id: 5,
             src: javascript,
             title: "JavaScript",
-            style: "shadow-yellow-500",
+            style: "yellow-500",
+            experties: 'Comfortable'
         },
         {
             id: 6,
             src: reactImage,
             title: "React",
-            style: "shadow-blue-600",
+            style: "blue-600",
+            experties: 'Expert'
         },
         {
             id: 9,
             src: github,
             title: "GitHub",
-            style: "shadow-gray-400",
+            style: "gray-400",
+            experties: 'Comfortable'
         },
         {
             id: 10,
             src: firebase,
             title: "Firebase",
-            style: "shadow-yellow-400",
+            style: "yellow-400",
+            experties: 'Authentication'
         },
         {
-            id: 10,
+            id: 12,
             src: redux,
             title: "Redux",
-            style: "shadow-purple-400",
+            style: "purple-400",
+            experties: 'Comfortable'
         },
         {
             id: 8,
             src: expressjs,
             title: "Express Js",
-            style: "shadow-yellow-400",
+            style: "yellow-400",
+            experties: 'Comfortable'
         },
         {
             id: 11,
             src: node,
             title: "Node Js",
-            style: "shadow-green-400",
+            style: "green-400",
+            experties: 'Comfortable'
+        },
+        {
+            id: 13,
+            src: mongodb,
+            title: "MongoDB",
+            style: "green-400",
+            experties: 'Comfortable'
         },
     ];
 
@@ -88,22 +107,26 @@ const Skills = () => {
         <div className="max-w-6xl mx-auto p-4 mt-8 md:mt-16 lg:mt-24 text-white">
             <SectionTitle description='These are the technologies I"ve worked with' sectionTitle='My' colorText='Skills' />
 
-            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center mt-14 sm:px-0">
-                {techs.map(({ id, src, title, style }) =>
-                    <div key={id} className="w-full h-[150px] [perspective: 1000px]">
-                        <div className={`w-full h-full relative rounded-md shadow-md ${style}`} key={id}>
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 text-center mt-14 sm:px-0">
+                {techs.map(({ id, src, title, style, experties }) =>
+                    <div key={id} className="group w-full h-[150px] [perspective:1000px]">
+                        <div className={`w-full h-full relative rounded-md shadow-md transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateX(180deg)] 
+                    shadow-${style}`} key={id}>
                             <div
-                                className="absolute inset-0"
+                                className='absolute inset-0'
                             >
-                                <img src={src} alt="" className="w-20 mx-auto" />
-                                <p className="mt-4">{title}</p>
+                                <img src={src} alt="" className="w-20 mx-auto group-hover:opacity-0 duration-500" />
+                                <p className="mt-4 group-hover:opacity-0 duration-500">{title}</p>
                             </div>
-                            <div className="absolute inset-0">
-                                <h1>helos</h1>
-                                <p>kldjfjfsl</p>
+                            <div className="absolute inset-0 flex flex-col justify-center items-center [transform:rotateX(180deg)] [backface-visibility:hidden]">
+                                <h1 className="text-xl font-bold block">
+                                    {experties}
+                                </h1>
+                                <p className="text-lg font-semibold block">{title}</p>
                             </div>
                         </div>
-                    </div>)}
+                    </div>
+                )}
             </div>
         </div>
     );
